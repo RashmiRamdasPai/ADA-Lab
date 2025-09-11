df03ab9c35a32228ff67f6732e8808b354cfafdf
Rashmi Ramdas Pai <161941092+RashmiRamdasPai@users.noreply.github.com>
2025-09-12 01:16:52 +0530

    Update floyds.c

--- floyds.c
+++ floyds.c
@@ -1,90 +1,72 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
-#include<limits.h>
 
-int n,opcount,dist[100][100];
-int i,j,k;
-int floyds(int mat[n][n],int n){
-	opcount=0;
-	for(i=0;i<n;i++)
-	  for(j=0;j<n;j++)
-	     if(mat[i][j]==-1)
-	        dist[i][j]=INT_MAX;
-	     else
-	        dist[i][j]=mat[i][j];
-	for(k=0;k<n;k++){
-		for(i=0;i<n;i++){
-			int temp=dist[i][k];
-		for(j=0;j<n;j++){
-			if(dist[i][j]>temp){
-				opcount++;
-			if(dist[k][j]!=INT_MAX && dist[i][j]>temp+dist[k][j])
-			  dist[i][j]=temp+dist[k][j];
-			  			}
-		}
-		}
-	}}
+int mat[100][100],n;
+int count=0;
+void create(int n){
+        for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                        if(i==j)
+                                mat[i][j]=0;
+                        else
+                                mat[i][j]=rand()%100+1;}}}
+
+void floyds(int mat[100][100],int n){
+        for(int k=0;k<n;k++){
+                for(int i=0;i<n;i++){
+                        for(int j=0;j<n;j++){
+                                count++;
+                                if(mat[i][k]!=99 && mat[k][j]!=99 && mat[i][k]+mat[k][j]<mat[i][j])
+                                        mat[i][j]= mat[i][k]+mat[k][j];}}}}
 void tester(){
-printf("\n Enter the number of vertices");
-scanf("%d",&n);
-int matrix[n][n];
-printf("\n Enter the matrix");
-for(i=0;i<n;i++)
-  for(j=0;j<n;j++)
-     scanf("%d",&matrix[i][j]);
-floyds(matrix,n);
-for(i=0;i<n;i++)
- {
- 	for(j=0;j<n;j++)
- 	  printf("%d",dist[i][j]);
- 	printf("\n");
- }
-	
-			
-		}
-		
+        printf("\n Enter the number of vertices");
+        scanf("%d",&n);
+        printf("\n Enter the cost matrix:");
+        for(int i=0;i<n;i++)
+                for(int j=0;j<n;j++)
+                        scanf("%d",&mat[i][j]);
+        floyds(mat,n);
+        printf("\n All pair shortest path:\n");
+        for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                        if(mat[i][j]==99)
+                                printf("INF ");
+                        else
+                                printf("%d ",mat[i][j]);
 
-	
+                }printf("\n");}
+}
+
+void plotter(){
+      FILE *fp=fopen("floyds.txt","a");
+      for(int k=2;k<=10;k++){
+              n=k;
+              count=0;
+              create(n);
+              floyds(mat,n);
+              fprintf(fp,"%d\t%d\n",n,count);}
+      fclose(fp);
+}
+void main(){
+        int ch;
+        for(;;){
+
+        printf("\n 1.Tester 2.Plotter 3.Exit \n");
+        printf("\n Enter your choice");
+        scanf("%d",&ch);
+        switch(ch){
+                case 1:tester();
+                       break;
+                case 2:plotter();
+                      break;
+                case 3:exit(0);
+            default:printf("\n Inavlid choice");
+        }
+}
+}
 
 	         
 	
 
 
 
-void plotter(){
-	FILE *f1 =  fopen("floyd.txt","a");
-	
-	for(k=2;k<12;k++){
-		n=k;
-		int mat[n][n];
-	  for(i=0;i<n;i++)
-	  	for(j=0;j<n;j++)
-	  		if(i!=j)
-	  		  mat[i][j]=rand()%n;
-	  		else 
-	  		  mat[i][j]=0;
-		  floyds(mat,n);
-		  
-		fprintf(f1,"%d\t%d\n",n,opcount);
-	  
-	}
-	fclose(f1);
-	
-}
-void main(){
-	int ch;
-	for(;;){
-	
-	printf("\n 1.Tester 2.Plotter 3.Exit \n");
-	printf("\n Enter your choice");
-	scanf("%d",&ch);
-	switch(ch){
-		case 1:tester();
-		       break;
-		case 2:plotter();
-		      break;
-		case 3:exit(0);
-	    default:printf("\n Inavlid choice");
-	}
-}}
