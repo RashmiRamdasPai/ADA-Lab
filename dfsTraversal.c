957271a5753e54b127b43ee5173ef79d221d1142
Rashmi Ramdas Pai <161941092+RashmiRamdasPai@users.noreply.github.com>
2025-09-11 23:00:56 +0530

    Update dfsTraversal.c

--- dfsTraversal.c
+++ dfsTraversal.c
@@ -1,114 +1,134 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int isCycle = 0, components = 0, n, opcount = 0, isTester = 0;
+int n, opcount = 0, top = -1;
 int i,j,k;
-int iscyclic=0;
-
-void dfs(int mat[n][n], int *vis, int source, int par)
+int dfs(int mat[n][n], int *vis, int *track, int source, int *stack)
 {
     vis[source] = 1;
-
-    if (isTester)
-        printf("%d ", source);
+    track[source] = 1;
 
     for ( i = 0; i < n; i++)
     {
         opcount++;
-        if (mat[source][i] && vis[i] && i != par)
-            isCycle = 1;
-        else if (mat[source][i] && !vis[i])
-            dfs(mat, vis, i, source);
+        if (mat[source][i] && track[i] && vis[i])
+        {
+            return 1;
+        }
+
+        if (mat[source][i] && !vis[i] && dfs(mat, vis, track, i, stack))
+        {
+            return 1;
+        }
     }
+
+    stack[++top] = source;
+    track[source] = 0;
+    return 0;
 }
 
-void checkConnectivity(int mat[n][n])
+int *checkConnectivity(int mat[n][n])
 {
-    int vis[n], k = 1;
+    int vis[n], track[n];
+    int* stack = (int*)malloc(n * sizeof(int));
 
     for ( i = 0; i < n; i++)
+    {
         vis[i] = 0;
+    }
 
-    for ( i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
+    {
         if (!vis[i])
         {
-            components++;
-
-            if (isTester)
-                printf("\nComponent %d: ", k++);
-            dfs(mat, &vis[0], i, -1);
+            if (dfs(mat, &vis[0], &track[0], i, stack))
+            {
+                return NULL;
+            }
         }
+    }
+
+    return stack;
 }
 
 void tester()
 {
-    isTester = 1;
-    printf("Enter the number of vertices\n");
+    printf("Enter number of vertices :\n");
     scanf("%d", &n);
     int adjMat[n][n];
+
     printf("Enter the adjacency matrix :\n");
     for ( i = 0; i < n; i++)
-        for (j = 0; j < n; j++)
+    {
+        for ( j = 0; j < n; j++)
+        {
             scanf("%d", &adjMat[i][j]);
+        }
+    }
 
-    checkConnectivity(adjMat);
-
-    printf("\nThe number of connected components :%d\n", components);
-
-    if (isCycle)
-        printf("Cycle exists\n");
+    int *stack = checkConnectivity(adjMat);
+    if (stack == NULL)
+    {
+        printf("Cycle exists..Cannot perform topological sorting!!!");
+        exit(0);
+    }
     else
-        printf("Cycle doesnot exists\n");
+    {
+        printf("Topological sorting order : \n");
+
+        while (top != -1)
+        {
+            printf("%d ", stack[top--]);
+        }
+    }
 }
 
-void plotter(){
-	isTester=0;
-	FILE *f1 = fopen("bfsbest.txt","a");
-	FILE *f2 = fopen("bfsworst.txt","a");
-	for(k=1;k<=10;k++){
-		n=k;
-		int mat[n][n];
-		//worst case:complete graph
-		for(i=0;i<n;i++){
-			for(j=0;j<n;j++){
-				if(i!=j)
-				  mat[i][j]=1;
-				else
-				 mat[i][j]=0;
-			}
-		}
-	
-	opcount=0;
-	iscyclic=0;
-	components=0;
-	checkConnectivity(mat);
-	fprintf(f1,"%d\t%d\n",n,opcount);
-	//best case:linear chain
-	for(i=0;i<n;i++)
-		for(j=0;j<n;j++)
-			mat[i][j]=0;
-	for(i=0;i<n-1;i++)
+void plotter()
 {
-		  mat[i][i+1]=1;
-		  mat[i+1][i]=1;
-		  }
-		  opcount=0;
-	iscyclic=0;
-	components=0;
-	checkConnectivity(mat);
-	fprintf(f2,"%d\t%d\n",n,opcount);
-	  
-}fclose(f1);
-fclose(f2);
-}
+    FILE *f1 = fopen("dfsTopSortworst1.txt", "w");
+    FILE *f2 = fopen("dfsTopSortbest1.txt", "w");
 
+    for (k = 1; k <= 10; k++)
+    {
+        n = k;
+        int adjMat[n][n];
+           //worst case upper triangle
+         for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++)
+                adjMat[i][j] = 0;
+
+        for (i = 0; i < n - 1; i++)
+        {
+            for (j = i + 1; j < n; j++)
+            {
+                adjMat[i][j] = 1;
+            }
+        }
+
+        opcount = 0, top = -1;
+        checkConnectivity(adjMat);
+        fprintf(f1,"%d\t%d\n", n, opcount);
+        //best case
+        for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+          adjMat[i][j]=0;
+        for(i=0;i<n-1;i++){
+        	adjMat[i][i+1]=1;
+        	
+		}
+		 opcount = 0, top = -1;
+        checkConnectivity(adjMat);
+        fprintf(f2,"%d\t%d\n", n, opcount);
+    }
+
+    fclose(f1);
+}
 
 void main()
 {
     int choice;
     printf("Enter\n1.Tester\n2.Plotter\n");
     scanf("%d", &choice);
-
     switch (choice)
     {
     case 1:
