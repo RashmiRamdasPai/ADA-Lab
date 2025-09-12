8d7f00ca07d2346b88db45c96a8ddcc00c5d9a63
Rashmi Ramdas Pai <161941092+RashmiRamdasPai@users.noreply.github.com>
2025-09-12 08:30:29 +0530

    Update dfsTraversal.c

--- dfsTraversal.c
+++ dfsTraversal.c
@@ -1,143 +1,80 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int n, opcount = 0, top = -1;
-int i,j,k;
-int dfs(int mat[n][n], int *vis, int *track, int source, int *stack)
-{
-    vis[source] = 1;
-    track[source] = 1;
 
-    for ( i = 0; i < n; i++)
-    {
-        opcount++;
-        if (mat[source][i] && track[i] && vis[i])
-        {
-            return 1;
-        }
+int iscyclic=0,connected=0,istester=0,count=0;
+int source,par,n;
+void dfs(int mat[n][n],int *vis,int source,int par){
+        vis[source]=1;
 
-        if (mat[source][i] && !vis[i] && dfs(mat, vis, track, i, stack))
-        {
-            return 1;
-        }
-    }
-
-    stack[++top] = source;
-    track[source] = 0;
-    return 0;
+         if(istester)
+                 printf("%d ",source);
+         for(int i=0;i<n;i++){
+                 count++;
+                 if(mat[source][i] && vis[i] && i!=par)
+                         iscyclic=1;
+                 else if(mat[source][i] && !vis[i])
+                         dfs(mat,vis,i,source);
+         }}
+void connectivity(int mat[n][n]){
+        int vis[n],k=1;
+        for(int i=0;i<n;i++)
+                vis[i]=0;
+        for(int i=0;i<n;i++){
+                if(!vis[i]){
+                        connected++;
+                        if(istester)
+                                printf("\n Component %d:",k++);
+                        dfs(mat,&vis[0],i,-1);
+                }}}
+void tester(){
+       printf("\n Enter the number of vertices");
+       scanf("%d",&n);
+       int mat[n][n];
+       printf("\n Enter the adjacency matrix:");
+       for(int i=0;i<n;i++)
+         for(int j=0;j<n;j++)
+            scanf("%d",&mat[i][j]);
+       istester=1;
+       connectivity(mat);
+       printf("\n The number of connected components %d ",connected);
+       if(iscyclic)
+               printf("\n Cycle exists");
+       else
+               printf("\n No cycle exists");
+}
+void plotter(){
+        istester=0;
+   FILE *f1=fopen("dfsbest.txt","a");
+   FILE *f2=fopen("dfsworst.txt","a");
+   for(int k=1;k<=10;k++){
+         n=k;
+         int mat[n][n];
+         //best case
+         count=0;
+         for(int i=0;i<n;i++)
+                 for(int j=0;j<n;j++)
+                         mat[i][j]=0;
+         for(int i=0;i<n-1;i++)
+                 mat[i][i+1]=1;
+         connectivity(mat);
+         fprintf(f1,"%d\t%d\n",n,count);
+         //worst case
+         count=0;
+         for(int i=0;i<n;i++)
+                 for(int j=0;j<n;j++)
+                         mat[i][j]=0;
+         for(int i=0;i<n;i++)
+                 for(int j=i+1;j<n;j++)
+                         mat[i][j]=1;
+         connectivity(mat);
+         fprintf(f2,"%d\t%d\n",n,count);
+   }fclose(f1);
+   fclose(f2);
 }
 
-int *checkConnectivity(int mat[n][n])
-{
-    int vis[n], track[n];
-    int* stack = (int*)malloc(n * sizeof(int));
-
-    for ( i = 0; i < n; i++)
-    {
-        vis[i] = 0;
-    }
-
-    for (i = 0; i < n; i++)
-    {
-        if (!vis[i])
-        {
-            if (dfs(mat, &vis[0], &track[0], i, stack))
-            {
-                return NULL;
-            }
-        }
-    }
-
-    return stack;
-}
-
-void tester()
-{
-    printf("Enter number of vertices :\n");
-    scanf("%d", &n);
-    int adjMat[n][n];
-
-    printf("Enter the adjacency matrix :\n");
-    for ( i = 0; i < n; i++)
-    {
-        for ( j = 0; j < n; j++)
-        {
-            scanf("%d", &adjMat[i][j]);
-        }
-    }
-
-    int *stack = checkConnectivity(adjMat);
-    if (stack == NULL)
-    {
-        printf("Cycle exists..Cannot perform topological sorting!!!");
-        exit(0);
-    }
-    else
-    {
-        printf("Topological sorting order : \n");
-
-        while (top != -1)
-        {
-            printf("%d ", stack[top--]);
-        }
-    }
-}
-
-void plotter()
-{
-    FILE *f1 = fopen("dfsTopSortworst1.txt", "w");
-    FILE *f2 = fopen("dfsTopSortbest1.txt", "w");
-
-    for (k = 1; k <= 10; k++)
-    {
-        n = k;
-        int adjMat[n][n];
-           //worst case upper triangle
-         for (i = 0; i < n; i++)
-            for (j = 0; j < n; j++)
-                adjMat[i][j] = 0;
-
-        for (i = 0; i < n - 1; i++)
-        {
-            for (j = i + 1; j < n; j++)
-            {
-                adjMat[i][j] = 1;
-            }
-        }
-
-        opcount = 0, top = -1;
-        checkConnectivity(adjMat);
-        fprintf(f1,"%d\t%d\n", n, opcount);
-        //best case
-        for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
-          adjMat[i][j]=0;
-        for(i=0;i<n-1;i++){
-        	adjMat[i][i+1]=1;
-        	
-		}
-		 opcount = 0, top = -1;
-        checkConnectivity(adjMat);
-        fprintf(f2,"%d\t%d\n", n, opcount);
-    }
-
-    fclose(f1);
-}
-
-void main()
-{
-    int choice;
-    printf("Enter\n1.Tester\n2.Plotter\n");
-    scanf("%d", &choice);
-    switch (choice)
-    {
-    case 1:
+int main(){
         tester();
-        break;
-    case 2:
         plotter();
-        break;
-    default:
-        printf("Invalid choice");
-    }
 }
+~                                                                                                                                                                                                                                                                                                                                                                                       ~            
